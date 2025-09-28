8c9866bbd17066af0f5f63207fee4272303579dc
danila.golovenko <dgolovenko01@gmail.com>
2025-09-29 01:17:41 +0400

    upd spawn tiles state

--- Assets/Scripts/Core/Features/LevelStatesFeature/States/SpawnTilesState.cs
+++ Assets/Scripts/Core/Features/LevelStatesFeature/States/SpawnTilesState.cs
@@ -1,6 +1,7 @@
 ﻿using Core.CommonComponents;
 using Core.Features.GameScreenFeature;
 using Core.Features.GameScreenFeature.Components;
+using Core.Features.GameScreenFeature.Mono;
 using Core.Features.TilesFeature;
 using Core.Features.TilesFeature.SimpleTile;
 using Core.Features.TilesFeature.TileWithInner;
@@ -10,6 +11,7 @@ using SelfishFramework.Src.Core;
 using SelfishFramework.Src.Core.Attributes;
 using SelfishFramework.Src.Core.Filter;
 using SelfishFramework.Src.StateMachine;
+using SelfishFramework.Src.Unity;
 using SelfishFramework.Src.Unity.Generated;
 using UnityEngine;
 
@@ -38,33 +40,59 @@ namespace Core.Features.LevelStatesFeature.States
                 ref var gridMonoProviderComponent = ref screenEntity.Get<GridMonoProviderComponent>();
                 ref var colorComponent = ref screenEntity.Get<ColorComponent>();
                 var grid = gridMonoProviderComponent.Grid;
-                if (grid.TryGetFreeCell(out var x, out var y, out var position))
+                for (int i = 0; i < Random.Range(1, 3); i++)
                 {
-                    //todo
-                    var color = _colorPaletteService.RandomColorFromCurrentPaletteExcept(colorComponent.Color);
-                    var simpleTileActor = _simpleTileFactoryService.GetTile(position, grid.transform, color);
-                    simpleTileActor.Entity.Set(new ColorComponent
+                    if (grid.TryGetFreeCell(out var x, out var y, out var position))
                     {
-                        Color = color,
-                    });
-                    var secondColor = _colorPaletteService.RandomColorFromCurrentPaletteExcept(colorComponent.Color, color);
-                    var tileActor = _tileWithInnerFactoryService.GetTile(position, grid.transform, secondColor, simpleTileActor);
-                    tileActor.Entity.Set(new ColorComponent
-                    {
-                        Color = secondColor,
-                    });
-                    tileActor.transform.localScale = Vector3.zero;
-                    tileActor.transform.DOScale(Vector3.one, 0.2f).SetLink(tileActor.gameObject);
-                    
-                    grid.Tiles[(x, y)] = tileActor.Entity;
-                    tileActor.Entity.Set(new GridPositionComponent
-                    {
-                        Position = new Vector2Int(x, y),
-                    });
+                        var tileActor = Random.value > 0.5
+                            ? CreateTileWithSimpleInner(colorComponent.Color, position, grid)
+                            : CreateSimpleTile(colorComponent.Color, position, grid);
+                        tileActor.transform.localScale = Vector3.zero;
+                        tileActor.transform.DOScale(Vector3.one, 0.2f).SetLink(tileActor.gameObject);
+
+                        grid.Tiles[(x, y)] = tileActor.Entity;
+                        tileActor.Entity.Set(new GridPositionComponent
+                        {
+                            Position = new Vector2Int(x, y),
+                        });
+                    }
                 }
+
                 EndState();
                 break;
             }
+        }
+
+        private Actor CreateTileWithSimpleInner(Color screenColor, Vector2 position, GridMonoComponent grid)
+        {
+            var color = _colorPaletteService.RandomColorFromCurrentPaletteExcept(screenColor);
+                    
+            var secondColor = _colorPaletteService.RandomColorFromCurrentPaletteExcept(screenColor, color);
+            var simpleTileActor = _simpleTileFactoryService.GetTile(position, grid.transform, secondColor);
+            simpleTileActor.Entity.Set(new ColorComponent
+            {
+                Color = secondColor,
+            });
+                    
+            var tileActor = _tileWithInnerFactoryService.GetTile(position, grid.transform, color, simpleTileActor);
+            tileActor.Entity.Set(new ColorComponent
+            {
+                Color = color,
+            });
+            return tileActor;
+        }
+        
+        private Actor CreateSimpleTile(Color screenColor, Vector2 position, GridMonoComponent grid)
+        {
+            var color = _colorPaletteService.RandomColorFromCurrentPaletteExcept(screenColor);
+                    
+            var tileActor = _simpleTileFactoryService.GetTile(position, grid.transform, color);
+            tileActor.Entity.Set(new ColorComponent
+            {
+                Color = color,
+            });
+                    
+            return tileActor;
         }
 
         public override void Exit(Entity entity)
